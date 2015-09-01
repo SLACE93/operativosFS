b5962d7d04a0ab62ccc776fdde9303eea88d7310
Cesar San Lucas <slace.93@gmail.com>
2015-08-31 19:29:39 -0500

    Agregado implementacion de tabla Hash para realizar la busqueda de una forma mas rapida

---
+++ HashTable.py
@@ -0,0 +1,79 @@
+__author__ = 'cesar17'
+class HashTable(object):
+
+    def __init__(self, M):
+        self.M = M          # tamano del HashTable
+        self.hashTable = []
+        for i in range(M):
+            sst = []   # sst lista para manejar las colisiones para un mismo hash
+            self.hashTable.append(sst)
+
+    def hash(self, key):
+        return (key.hashCode() & 0x7fffffff) % self.M
+
+    def getValue(self, key):
+        sst = self.hashTable[self.hash(key)]
+        return self.getValueInner(sst,key)
+
+    def getValueInner(self, sst, key):
+        if sst:
+            for keyvalue in sst:
+                keyaux = keyvalue.getKey()
+                if key == keyaux:
+                    return keyvalue.getValue()
+        return None
+
+    def deleteKey(self, key):
+        sst = self.hashTable[self.hash(key)]
+        if sst:
+            count = 0
+            for keyvalue in sst:
+                keyaux = keyvalue.getKey()
+                if key == keyaux:
+                    return sst.pop(count)
+                    #break
+                count = count + 1
+        return None
+
+    def putKeyValue(self, key, value):
+        sst = self.hashTable[self.hash(key)]
+        self.putInner(sst, key, value)
+
+    def putInner(self, sst, key, value):
+        keyvalue = KeyValue(key, value)
+        sst.append(keyvalue)
+
+
+class KeyValue(object):
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def getKey(self):
+        return self.key
+
+    def getValue(self):
+        return self.value
+
+    def setValue(self, newValue):  #Solo para cambiar referencia al indice de la Tabla de pagina
+        self.value = newValue
+
+
+class StringMH(object):
+    def __init__(self, string):
+        self.string = string
+        self.hash = 0
+
+    def hashCode(self):
+        h = self.hash
+        if h!=0:
+            return self.hash
+        for c in self.string:
+            h = ord(c) + (31 * h)
+        self.hash = h
+        return h
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.string == other.string
+        return False
